a021839c88b5af3b3f2ff95a8e0258eb89cdf507
Josh Perez <josh@goatslacker.com>
2011-08-26 17:59:07 -0700

    more enlightenment

--- 5/haskell.hs
+++ 5/haskell.hs
@@ -1,6 +1,9 @@
 -- I'm such a Haskell n00b. This program runs much slower than it's JavaScript translation.
 -- I know it's a case of I'm doing it wrong. I'd like to find the right path to englightenment and Haskell glory.
 
+-- Seems like running it in ghci is a bad idea. Compile it, duh!
+-- Although I'm sure I can make better use of lists in this case anyways
+
 increment :: Int -> Int -> Int -> Int
 increment number i range = if number `mod` i == 0
                             then divideByRange number (i + 1) range
@@ -14,4 +17,4 @@ divideByRange number i range = if i == range
 findSmallest :: Int -> Int -> Int
 findSmallest number range = divideByRange number 1 range
 
---findSmallest 2520 20
+main = print (findSmallest 2520 20)
