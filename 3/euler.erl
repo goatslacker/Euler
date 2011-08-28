1b25284abf6e6a95683d14e3fae3df1a2c8fbde5
Josh Perez <josh@goatslacker.com>
2011-08-27 22:22:04 -0700

    erlang solution to euler 3

---
+++ 3/euler.erl
@@ -0,0 +1,39 @@
+-module(euler).
+
+-export([getLargestPrimeFactor/1]).
+
+% gets a list range from 1..sqrt
+getRange(Num) ->
+  lists:seq(1, round(math:sqrt(Num)-0.5)).
+
+% gets multiples up to a certain extent
+getMultiples(Num) ->
+  [X || X <- getRange(Num), Num rem X == 0].
+
+% bool. is number prime || not
+isPrime(Num) ->
+  Length = length(getMultiples(Num)),
+  Result = if Length > 1 -> false;
+              true -> true
+            end,
+  Result.
+
+% get primes get's all the prime numbers
+getPrimes(Num) ->
+  [X || X <- getRange(Num), isPrime(X) == true].
+
+% bool. if it's a prime factor || not
+isPrimeFactor(Num, Base) ->
+  Result = if Base rem Num == 0 -> true;
+              true -> false
+            end,
+  Result.
+
+% returns all prime factors in a list
+getPrimeFactors(Num) ->
+  [X || X <- getPrimes(Num), isPrimeFactor(X, Num) == true].
+
+% returns the largest prime factor for a number
+getLargestPrimeFactor(Num) ->
+  PrimeFactors = getPrimeFactors(Num),
+  lists:last(PrimeFactors).
--- 3/haskell.hs
+++ 3/haskell.hs
@@ -1,35 +1,4 @@
--- Used to check if it's a prime || primefactor
--- loop is the loop callback
--- end is the callback for else
-iterator num i loop end = if i * i < num
-                                then loop
-                                else end
-
-checkPrime num i = iterator num i (canBePrime num i) True
-
-canBePrime num i = if num `mod` i == 0 && num /= i
-                  then False
-                  else checkPrime num (i + 1)
-
--- Bool
-isPrime num = canBePrime num 2
-
--- Bool
-isPrimeFactor num base = if base `mod` num == 0
-                          then True
-                          else False
-
--- Bool
-isPrimeAndPrimeFactor num base = if isPrime num == True && isPrimeFactor num base == True
-                                  then True
-                                  else False
-
-findPrimeFactors num i largest = if isPrimeAndPrimeFactor i num == True
-                                  then getLargestPrimeFactor num (i + 1) i
-                                  else getLargestPrimeFactor num (i + 1) largest
-
-getLargestPrimeFactor num i largest = iterator num i (findPrimeFactors num i largest) largest
-
-getLargestPrimeFactorOf num = getLargestPrimeFactor num 2 0
-
--- getLargestPrimeFactorOf 600851475143
+--getD n = getD' n 2 where
+--  getD' n f | n == f = f
+--            | n `mod` f == 0 = getD' (n `div` f) f
+--            | otherwise = getD' n (succ f)
