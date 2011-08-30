26a01303b32a7dbd5f4a46124bceef9bad3fd141
Josh Perez <josh@goatslacker.com>
2011-08-30 03:12:37 -0700

    erlang solution to 10

---
+++ 10/euler.erl
@@ -0,0 +1,23 @@
+-module(euler).
+
+-export([getSumOfPrimes/1]).
+
+% gets a list range from 1..sqrt
+getRange(Num) ->
+  lists:seq(1, round(math:sqrt(Num)-0.5)).
+
+% gets multiples up to a certain extent
+getMultiples(Num) ->
+  [X || X <- getRange(Num), Num rem X == 0].
+
+% bool. is number prime || not
+isPrime(Num) ->
+  Length = length(getMultiples(Num)),
+  Result = if Length > 1 -> false;
+              true -> true
+            end,
+  Result.
+
+% get primes get's all the prime numbers
+getSumOfPrimes(Max) ->
+  lists:sum([X || X <- lists:seq(2, Max), isPrime(X) == true]).
