e9f22daba7dfc5ea313892938270f7002037a2c5
Josh Perez <josh@goatslacker.com>
2011-09-02 22:58:29 -0700

    renaming functions, oops

--- 3/euler.erl
+++ 3/euler.erl
@@ -7,12 +7,12 @@ getRange(Num) ->
   lists:seq(1, round(math:sqrt(Num)-0.5)).
 
 % gets multiples up to a certain extent
-getMultiples(Num) ->
+getFactors(Num) ->
   [X || X <- getRange(Num), Num rem X == 0].
 
 % bool. is number prime || not
 isPrime(Num) ->
-  Length = length(getMultiples(Num)),
+  Length = length(getFactors(Num)),
   Result = if Length > 1 -> false;
               true -> true
             end,
